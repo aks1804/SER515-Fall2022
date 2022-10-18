e1a53b7d45e21998cb0083f14b1a200837cf7cda
Akshat <Akshat@MacbookPro>
2022-10-17 18:28:12 -0700

    Skeleton Code for PTSB

---
+++ Person.java
@@ -0,0 +1,25 @@
+public class Person {
+
+	private ProductMenu theProductMenu;
+
+	public abstract void showMenu();
+
+	public void showAddButton() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public abstract ProductMenu createProductMenu();
+
+}
